e7d61c3f2e96ef9e50d4030e500a1a3df3166092
Aakash Kavediya <kavediyaaakash@gmail.com>
2021-05-30 16:43:15 +0530

    push all elements

--- skech.js
+++ skech.js
@@ -3,26 +3,36 @@ var player, background, playerman, play;
 function preload() {
     back = loadImage("img/space-1.gif");
     iron = loadImage("img/iron.png");
-    thor = loadImage("img/cat.png")
+    thor = loadImage("img/cat.png");
+    fire = loadImage("img/fire.png");
+    first = loadImage("img/first.png");
+    huk = loadImage("img/hb.png");
+    hulk = loadImage("img/hulk.png");
+    ion = loadImage("img/ion.png");
+    thanos = loadImage("img/thanos.png");
+    space = loadImage("img/space.png");
 }
 
 function setup() {
-    createCanvas(1200, 1000);
-    man = createSprite(625, 280);
+    createCanvas(1000, 900);
+    man = createSprite(500, 280);
     man.scale = 2.5;
-    man.addImage(back);
-
+    man.addImage(space);
 
     edge = createEdgeSprites();
-    player = createSprite(200, 900, );
-    player.addImage(iron);
-    player.scale= .14;
+    player = createSprite(200, 500);
+    player.addImage(huk);
+    player.scale= .2;
 
-    play = createSprite(1000,900);
-    play.addImage(thor);
-    play.scale=.4;
+    // obs = createSprite(200,200);
+    // obs.addImage(thanos);
+    // obs.scale = .1;
+    // player.visible = true
 
-
+    // obs1 = new Group();
+    // obs2 = new Group();
+    // obs3 = new Group();
+    // obs4 = new Group();
 }
 
 function draw() {
@@ -30,28 +40,39 @@ function draw() {
     drawSprites();
 
     if (keyDown("w")) {
-        player.y = player.y - 20;
+        player.y = player.y - 10;
+        man.velocityY = 5
     }
     if (keyDown("s")) {
-        player.y = player.y + 20;
+        player.y = player.y + 10;
+        man.velocityY = -5
     }
     if (keyDown("a")) {
-        player.x = player.x - 20;
+        player.x = player.x - 10;
+        man.velocityX = 5
     }
     if (keyDown("d")) {
-        player.x = player.x + 20;
+        player.x = player.x + 10;
+        man.velocityX = -5
     }
-    if (keyDown("up")) {
-        play.y = play.y - 20;
+    if(keyDown("space")){
+        player.y = player.y - 20;
+        man.velocityY = 10;
+        // createSprite(50,50);
+        // fan.addImage(fire);
+        // fan.scale = 2;
     }
-    if (keyDown("down")) {
-        play.y = play.y + 20;
+    if(man.y>1000){
+        man.y = man.height/4
     }
-    if (keyDown("left")) {
-        play.x = play.x - 20;
+    if(man.y<200){
+        man.y = man.height/3
     }
-    if (keyDown("right")) {
-        play.x = play.x + 20;
+    if(man.x>1000){
+        man.x = man.width/3
+    }
+    if(man.x<100){
+        man.x = man.width/3
     }
 
     player.bounceOff(edge[0]);
@@ -59,18 +80,7 @@ function draw() {
     player.bounceOff(edge[2]);
     player.bounceOff(edge[3]);
 
-    play.bounceOff(edge[0]);
-    play.bounceOff(edge[1]);
-    play.bounceOff(edge[2]);
-    play.bounceOff(edge[3]);
 
-    if(player.isTouching(play)) {
-        textSize(40)
-        text("You Crash", 550,480)
-    }
-    
-    if(play.isTouching(player)) {
-        textSize(40)
-        text("You Crash", 550,480)
-    }
+   
 }
+
